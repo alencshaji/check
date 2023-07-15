af2105204acbd421987d12353cedfbbf7381264b
alencshaji <alencshaji@gmail.com>
2023-07-15 15:59:12 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,21 @@
+
+function sum() {
+    num1=parseInt(number_1.value)
+    num2=parseInt(number_2.value)
+    alert(num1+num2)
+}
+function sub() {
+    num1=parseInt(number_1.value)
+    num2=parseInt(number_2.value)
+    alert(num1-num2)
+}
+function div() {
+    num1=parseInt(number_1.value)
+    num2=parseInt(number_2.value)
+    alert(num1/num2)
+}
+function mul() {
+    num1=parseInt(number_1.value)
+    num2=parseInt(number_2.value)
+    alert(num1*num2)
+}
